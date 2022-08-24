5f89030c48bec6b65555a9aeaed52f043e619cec
Andrés León <88801431+TiltRex@users.noreply.github.com>
2022-08-24 11:25:46 -0500

    Update Numeros Pseudoaleatorios.py

--- Numeros Pseudoaleatorios.py
+++ Numeros Pseudoaleatorios.py
@@ -1,12 +1,13 @@
-
 n=int(input("Cuantos numeros desea generar: "))
 num=int(input("En que numero quiere inciar: "))
 
 a=int(input("Defina el multiplicador: "))
 c=int(input("Defina el desface: "))
 
+list=[]
 
 for x in range(n):
-    print(num)
+    list.append(num)
     num=(a*num+c)%1000
-    
+
+print(list)

4927ef22fce07a6657811e844867075e39a146cb
Andrés León <88801431+TiltRex@users.noreply.github.com>
2022-08-24 11:22:59 -0500

    Update Euclides_Extendido.py

--- Euclides_Extendido.py
+++ Euclides_Extendido.py
@@ -1,9 +1,11 @@
 a=int(input("Numero 1: "))
 b=int(input("Numero 2: "))
 
+print("\n")
+
 num1=a
 num2=b
- 
+
 x0 = 1
 x1 = 0
 y0 = 0
@@ -15,6 +17,8 @@ while b != 0:
     x = x0 - q * x1
     y = y0 - q * y1
 
+    print(f"{a}      {b}      {x0}      {y0}")
+
     a = b
     b = r
 
@@ -23,4 +27,5 @@ while b != 0:
     y0 = y1
     y1 = y
 
-print(f"{a} = {num1}({x0}) + {num2}({y0})")
+
+print(f"\n{a} = {num1}({x0}) + {num2}({y0})")
